57f6e0d1af5678cba88394fe365e4ef0f3b69f49
Ritik Saxena <ritiksaxena124@gmail.com>
2022-08-20 13:43:32 +0530

    completed password generator

---
+++ index.js
@@ -0,0 +1,112 @@
+let generatePass = document.getElementById("generate-pass");
+let password1 = document.getElementById("password1");
+let password2 = document.getElementById("password2");
+
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+function generatePasswords() {
+  let firstPass = "";
+  let secondPass = "";
+  for (let i = 0; i < 16; i++) {
+    let indexForPass1 = Math.floor(Math.random() * characters.length);
+    let indexForPass2 = Math.floor(Math.random() * characters.length);
+
+    firstPass += characters[indexForPass1];
+    secondPass += characters[indexForPass2];
+  }
+
+  password1.textContent = firstPass;
+  password2.textContent = secondPass;
+}
